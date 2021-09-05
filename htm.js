53f8efe6774d6ad119008feea6b50e641fb1af6c
86135 <63903899+86135@users.noreply.github.com>
2021-09-05 12:51:34 +0800

    Add files via upload

---
+++ htm.js
@@ -0,0 +1,4 @@
+chrome.storage.local.get({xueshu:"y"},(res)=>{document.querySelector("#open").checked=(res.xueshu==="y");})
+function submit(){inp=document.querySelector("#open");chrome.storage.local.set({xueshu:inp.checked?"y":"n"},()=>{alert("ok");});}
+document.querySelector("#submit").addEventListener("click",submit);
+//论box3与前端的关系
